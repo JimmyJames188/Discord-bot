64c8fb0e42cfd875533123bb5c09dd7112b70e1a
Kirill-iceland <58794914+Kirill-iceland@users.noreply.github.com>
2021-02-17 16:13:24 +0000

    Merge changes

--- commands/Sundleikurinn.js
+++ commands/Sundleikurinn.js
@@ -1,12 +1,6 @@
 const fs = require('fs');
-
 const Discord = require("discord.js");
-
-<<<<<<< HEAD
-const Drive = require("C:/Users/james/Desktop/Discord Bot/Storage/Drive.js")
-=======
 const Drive = require("..\\Storage\\Drive.js")
->>>>>>> 1b777a30f1c14f147a801fdffaa4bb4f261cac42
 
 
 const delay = 1000
