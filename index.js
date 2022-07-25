90b52f1d544315b9437fe59e5523bf1d98568280
JimmyJames188 <64692520+JimmyJames188@users.noreply.github.com>
2022-07-25 10:58:05 +0000

    ah fuck, ruh roh and nice

--- index.js
+++ index.js
@@ -214,9 +214,13 @@ bot.on('message', async msg=> {
     }else if(msg.content === "!racist-info"){
         msg.reply('https://youtu.be/USE86UbsV8c');
 
-    }else if(msg.content === "!nice"){
-        msg.reply('https://youtu.be/ffQmb-cNFuk');
+    }else if(msg.content === "nice"){
+        msg.channel.send('https://c.tenor.com/hnP8FWI5fB4AAAAC/joseph-joestar-nice.gif');
 
+    }else if(msg.content === "ruh roh"){
+        msg.channel.send('https://tenor.com/x2Yc.gif');
+
+    
     }else if(msg.content === "Jæja þá skulum við fara með bæn"){
         msg.reply('Heilagi Gummi! Lífið og heilsan er helgidómur, sem þú hefur gefið okkur, því er allt heilagt sem viðheldur lífinu. Gef okkur því visku til að njóta matar og drykkjar með fögnuði og þakklátum huga og gæta jafnframt hófs. Blessaðu líf okkar og starf og gef okkur náð og þrótttil að vera trúir þjónar á akri þínum. Send snauðum og sjúkum hjálp og syrgjendum huggun. Blessaðu og helgaðu lífið sérhverja stund í Gumma krists heilaga nafni. A M E N');
 
@@ -371,6 +375,24 @@ bot.on("message", async message => {
         message.channel.send("Hver sagði þetta!?");
     }
 
+    blacklisted = ["yo mom"];
+    for (var i in blacklisted) {
+        if (message.content.toLowerCase().includes(blacklisted[i].toLowerCase())) foundInText = true;
+    }
+
+    if (foundInText) {
+        message.channel.send("https://c.tenor.com/mYbI09S93U8AAAAd/supa-hot-fire-ooooh.gif");
+    }
+
+
+    blacklisted = ["Ah fuck"];
+    for (var i in blacklisted) {
+        if (message.content.toLowerCase().includes(blacklisted[i].toLowerCase())) foundInText = true;
+    }
+
+    if (foundInText) {
+        message.channel.send("https://tenor.com/bEhW8.gif");
+    }
 
     blacklisted = ["IS JAMES BANANAMAN"];
 
